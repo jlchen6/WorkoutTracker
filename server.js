a62ef86588f82d3458ec6b86c4811f63836273c6
Julia <jlchen6@wisc.edu>
2020-04-24 16:41:53 -0500

    Setting up files

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
 
-const db = require("./models");
-
 const app = express();
+var PORT = process.env.PORT || 3000;
 
-app.use(logger("dev"));
-
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/imageperformance", {
+  useNewUrlParser: true
+});
+
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+});
